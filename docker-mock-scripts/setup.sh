548dee567db66cc6dd577d518642a564a330fa23
David Golembiowski <dmgolembiowski@gmail.com>
2020-05-14 01:02:11 -0400

    fixed double root check

--- docker-mock-scripts/setup.sh
+++ docker-mock-scripts/setup.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 echo -e "\e[30;48;5;82mEDBPool E-Z Setup\e[0m"
-
-if [[ $(id -u) -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
-fi
-
 echo -e "\e[40;38;5;82mBeginning RPC/Reverse Shell Pipeline:"
 
 cd /home/edbpool/edbpool \
