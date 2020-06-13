665eefd31a12e6c469e2eef9e588e169e475ec9e
David Golembiowski <dmgolembiowski@gmail.com>
2020-06-13 10:10:36 -0400

    fallback before testing

--- modules/experimenting/server.py
+++ modules/experimenting/server.py
@@ -1,12 +1,28 @@
 import edgedb
 import asyncio
+from async_timeout import timeout
 import contextvars
 import functools
 from edgedb import create_async_pool
-from .env import Config
+from env import Config
 from enum import Flag, auto
 from secrets import token_hex
 import uvicorn
+
+from quart import Quart, request, abort
+
+class AdaptedQuart(Quart):
+    def __init__(self, name):
+        self._edbconfig = {}
+        super().__init__(name)
+
+    def __getitem__(self, key):
+        return self._edbconfig[key]
+    
+    def __setitem__(self, key, value):
+        self._edbconfig[key] = value
+
+app = AdaptedQuart(__name__)
 
 """
 Global namespace of mutable context vars
@@ -16,12 +32,17 @@ Global namespace of mutable context vars
 # `aiopool_ctx.set(<var>)`, the Python runtime
 # will magically track all of the reference counts 
 # to an underlying `AsyncioPool` connection.
-aiopool_ctx = contextvars.ContextVar('aiopool')
+# global aiopool_ctx
+# aiopool_ctx = contextvars.ContextVar('aiopool')
 
-
+'''
 # Depending on `RequestKind`, `edb_result` might be an
 # `asyncio.Future` that anticipates an EdgeSet, EdgeTuple, or JSON.
 edb_result_ctx = contextvars.ContextVar('edb_result')
+'''
+
+# Request-specific query
+statement_ctx = contextvars.ContextVar('statement')
 
 # A boolean value indicating whether we create transactions
 use_transactions_ctx = contextvars.ContextVar('transact')
@@ -45,12 +66,15 @@ fetch_exec_kind = contextvars.ContextVar('fetchEx')
 class FetchExecKind(Flag):
     """class FetchExecKind(enum.Flag):
     A prototype extracted from the request's data
-    body. If `FetchExecKind` is not explicitly
-    defined in the body as any of the following 
-    options:
+    body. Because `edgedb.Set` instances do not 
+    directly support pickling or binary object serialization,
+    it doesn't make sense to allow the client to call the 
+    `fetchone` or `fetchall` methods. 
+    If `FetchExecKind` is not explicitly defined in the body 
+    as any of the following options:
         1.) "FetchExecKind":"execute"
-        2.) "FetchExecKind":"fetchone_json"
-        3.) "FetchExecKind":"fetchall_json"
+        3.) "FetchExecKind":"fetchone_json"
+        4.) "FetchExecKind":"fetchall_json"
     then the connection will default to using 
     `fetchall_json` when formatting the database's
     return to the client.
@@ -61,7 +85,7 @@ class FetchExecKind(Flag):
     fetchall      = auto()
     fetchall_json = auto()
 
-async def handle_request(statement):
+async def handle_request(response_future):
     """async def handle_request(request: dict) -> None
     This method dispatches the request prepared by
     the client.
@@ -69,10 +93,8 @@ async def handle_request(statement):
     will be returned."""
     fetchEx     = fetch_exec_kind.get('fetchEx')
     transact    = use_transactions_ctx.get('transact')
-    aiopool     = aiopool_ctx.get('aiopool')
-    loop        = asyncio.get_running_loop()
-    db_response = loop.create_future()
-    connection  = aiopool.acquire()
+    statement   = statement_ctx.get('statement')
+    connection  = app["pool"].acquire()
 
     # Initialize some names and handlers for retaining
     # DB responses
@@ -81,20 +103,17 @@ async def handle_request(statement):
         async with connection.transaction():
             if fetchEx & FetchExecKind.fetchall_json:
                 res = await connection.fetchall_json(statement)
-            elif fetchEx & FetchExecKind.fetchall:
-                res = await connection.fetchall(statement)
             elif fetchEx & FetchExecKind.fetchone_json:
                 res = await connection.fetchone_json(statement)
-            elif fetchEx & FetchExecKind.fetchone:
-                res = await connection.fetchone(statement)
             elif fetchEx & FetchExecKind.execute:
                 res = await connection.execute(statement)
             else:
-                raise ValueError("""Client did not specify a valid
+                abort(400, description="""Client did not specify a valid
                 execution kind. Aborting the transaction.""")        
             try:
+                # Nested transaction for the savepoint
                 async with connection.transaction():
-                    await connection.execute("SELECT {0}")
+                    await connection.execute("select 0;")
                     raise Exception
             except:
                 pass
@@ -102,16 +121,14 @@ async def handle_request(statement):
             await aiopass()
             if res is not None:
                 try:
-                    db_response.set_result(res)
+                    response_future.set_result(res)
                 except asyncio.CancelledError:
-                    raise asyncio.CancelledError("""
+                    abort(400, description="""
                     Attempted to set the result of a
-                    db_response future when it had
+                    response_future future when it had
                     already been marked as completed.
-                    """
-                )
-                finally:
-                    break
+                    """)
+                break
             else:
                 await aiopass()
 
@@ -119,16 +136,12 @@ async def handle_request(statement):
         async with connection:
             if fetchEx & FetchExecKind.fetchall_json:
                 res = await connection.fetchall_json(statement)
-            elif fetchEx & FetchExecKind.fetchall:
-                res = await connection.fetchall(statement)
             elif fetchEx & FetchExecKind.fetchone_json:
                 res = await connection.fetchone_json(statement)
-            elif fetchEx & FetchExecKind.fetchone:
-                res = await connection.fetchone(statement)
             elif fetchEx & FetchExecKind.execute:
                 res = await connection.execute(statement)
             else:
-                raise ValueError("""
+                abort(400, description="""
                 Client did not specify a valid
                 execution kind. Aborting the 
                 transaction.""")    
@@ -139,22 +152,65 @@ async def handle_request(statement):
             else:
                 await aiopass()
         try:
-            db_response.set_result(res)
+            response_future.set_result(res)
         except asyncio.CancelledError as e:
-            raise asyncio.CancelledError("""
+            abort(400, description="""
             Attempted to set the result of a 
-            db_response future when it had
+            response_future future when it had
             already been marked as completed.
             """
         )
-        return db_response
     else:
         # Assume that `transact` was never set
         # in the first place properly by this
         # server, and throw a generic exception.
-        raise Exception("Transaction kind (`true` or `false`) was never properly set. Exiting.")
+        abort(400 description="""Transaction kind (`true` or `false`) 
+        was never properly set. Exiting.""")
 
 async def aiopass():
     await asyncio.sleep(0.0000001)
 
+@app.route('/', methods=['POST'])
+async def recv():
+    data = None
+    loop = asyncio.get_running_loop()
+    database_response_future = loop.create_future()
+    async with timeout(Config.BODY_TIMEOUT):
+        data = request.get_data()
+    while True:
+        await aiopass()
+        if data is not None:
+            # Set context vars with data keys.
+            # Return '{"error": -1}' if
+            # everything required was not present
+            try:
+                transact.set(UseTransactions.true if data["UseTransactions"] else UseTransactions.false)
+                fetchEx.set(
+                    FetchExecKind.fetchall_json if data["FetchExecKind"] == "fetchall_json" else
+                        FetchExecKind.fetchone_json if data["FetchExecKind"] == "fetchone_json" else
+                            FetchExecKind.execute
+                )
+                statement.set(data["statement"])
+                await handle_request(database_response_future)
+                await database_future_response
+                return database_future_response.get()
+            except KeyError:
+                return abort(400, description="""
+                The client's POST request body did not contain
+                the sufficient data.""")
+    
+@app.before_serving
+async def create_db_pool():
+    app["pool"] = await edgedb.create_async_pool(
+        dsn=Config.dsn(),
+        min_size=Config.min_size,
+        max_size=Config.max_size
+    )
+
+if __name__ == "__main__":
+    app.run(
+        host=Config.host,
+        port=Config.port,
+        debug=True
+    )
 
